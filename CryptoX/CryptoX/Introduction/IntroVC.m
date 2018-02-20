431aafb0adac1aad3d19ba16caa73b977f598ab9
Wilmer sinchi <wilmersinchi@gmail.com>
2018-02-20 16:15:06 -0500

    introduction initial

---
+++ CryptoX/CryptoX/Introduction/IntroVC.m
@@ -0,0 +1,37 @@
+//
+//  IntroVC.m
+//  CryptoX
+//
+//  Created by Wilmer sinchi on 2/20/18.
+//  Copyright © 2018 YAUHENI IVANIUK. All rights reserved.
+//
+
+#import "IntroVC.h"
+
+@interface IntroVC ()
+
+@end
+
+@implementation IntroVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
